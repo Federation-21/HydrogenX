e631fb20e433974675cd099bc04f643d7c16d042
shakilofficial0 <shakilofficial0@gmail.com>
2023-07-13 15:06:52 +0600

    init backend with vulnab

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Routes;
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+$controller_path = 'App\Http\Controllers';
 
+// Main Page Route
+// Route::get('/', $controller_path . '\pages\HomePage@index')->name('pages-home');
+Route::get('/page-2', $controller_path . '\pages\Page2@index')->name('pages-page-2');
+
+// pages
+Route::get('/pages/misc-error', $controller_path . '\pages\MiscError@index')->name('pages-misc-error');
+
+// authentication
+Route::get('/auth/login-basic', $controller_path . '\authentications\LoginBasic@index')->name('auth-login-basic');
+Route::get('/auth/register-basic', $controller_path . '\authentications\RegisterBasic@index')->name('auth-register-basic');
 Route::get('/', [Routes::class, 'index'])->name('index');
 Route::get('about', [Routes::class, 'about'])->name('about');
 Route::get('terms-of-service', [Routes::class, 'tos'])->name('tos');
